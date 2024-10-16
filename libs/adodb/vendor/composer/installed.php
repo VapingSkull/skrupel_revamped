ef3d35f0254593bfb9886245ba138685a7e8d0ee
VapingSkull <cash4read@gmx.de>
2024-10-16 17:35:36 +0200

    smarty und adodb
    Smarty Template Engine und Adodb in der jeweils aktuellen Version integriert

---
+++ libs/adodb/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'adodb/adodb-php' => array(
+            'pretty_version' => 'v5.22.7',
+            'version' => '5.22.7.0',
+            'reference' => 'e7150539d5707ae556172532e2b25ac4e88cb2a6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../adodb/adodb-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
