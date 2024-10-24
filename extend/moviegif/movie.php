1056aeb6a56f6d159a7ba38339b2704ab6100e75
VapingSkull <cash4read@gmx.de>
2024-10-24 12:14:50 +0200

    Datei überarbeite
    Neue Datenbankanbindung integriert
    Datei Pfade korrigiert.
    Überflüssige Leerzeilen entfernt

--- extend/moviegif/movie.php
+++ extend/moviegif/movie.php
@@ -1,59 +1,33 @@
 <?php
 
-$game_id = (int) $_GET['game'];
-
-include (dirname(__FILE__).'/../../inc.conf.php');
-include (dirname(__FILE__).'/../../inhalt/inc.hilfsfunktionen.php');
-open_db();
-
-$zeiger = @mysql_query("SELECT phase FROM skrupel_spiele WHERE phase = 1 AND id = $game_id LIMIT 1");
-
-if (@mysql_num_rows($zeiger) == 1) {
-
-    $moviegif_files_path = dirname(__FILE__).'/files/moviegif/';
+$game_id = $_GET['game'];
+$zeiger = $db->getOne("SELECT phase FROM skrupel_spiele WHERE phase='1' AND id='".$game_id."' LIMIT 1");
+if (isset($zeiger)) {
+    $moviegif_files_path = main_dir . 'files/moviegif/';
     $movie_file = $moviegif_files_path . 'movies/moviegif_' . $game_id . '.gif';
-
     if (!@file_exists($movie_file))  {
-
         @file_put_contents($movie_file, '');
-
         $scenes_dir = $moviegif_files_path . 'temp/' . $game_id . '/';
-
         foreach (scandir($scenes_dir) as $object) {
 	        if ($object[0] != '.') {
 		        $scenes[] = $scenes_dir . $object;
 			    $scenes_dur[] = 120;
 	        }
         }
-
         sort($scenes);
-
         // create
-
         include 'gifencoder.class.php';
-
         $gif = new GifEncoder($scenes, $scenes_dur, 0, 2, 255, 0, 0, 'url');
-
         @file_put_contents($movie_file, $gif->getAnimation());
         @chmod($movie_file, 0777);
-
         // cleanup
-
         foreach ($scenes as $scene) {
 		    unlink($scene);
         }
-
         rmdir($scenes_dir);
-
     }
-
     // output
-
     header('Content-Type: image/gif');
     echo @file_get_contents($movie_file);
-
 }
 
-@mysql_close($conn);
-
-?>
